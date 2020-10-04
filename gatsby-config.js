3938fa5859eed9b72d3708fbad1351751f366bbe
Hamza Bargaz <hamza.bargaz@gmail.com>
2020-10-05 00:09:42 +0100

    add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: ``, // This path is relative to the root of the site.
+        icon: `src/assets/images/developer.png`, // This path is relative to the root of the site.
       },
     },
     {
