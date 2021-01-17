98ff2b288c504866bee09b61c1414e5c99477f04
Hamza Bargaz <hamza.bargaz@gmail.com>
2021-01-17 21:54:04 +0100

    add confirmation vars

--- src/pages/blog/card.js
+++ src/pages/blog/card.js
@@ -6,19 +6,19 @@ import cx from "classnames"
 export default ({ node }) => {
   return (
     <div className="flex flex-col p-4 group box">
-      <Link to={`/${node.frontmatter.slug}`}>
+      <Link to={`/${node?.frontmatter?.slug}`}>
         <div className="flex flex-wrap items-center">
           <Img
-            fluid={node.frontmatter.hero.childImageSharp.fluid}
+            fluid={node?.frontmatter?.hero?.childImageSharp?.fluid}
             alt=""
             className="w-full md:w-1/2"
           />
           <div className="w-full md:w-1/2 flex flex-col p-4">
             <div className="text-3xl text-secondary-base mb-2">
-              {node.frontmatter.title}
+              {node?.frontmatter?.title}
             </div>
             <p className="mb-2 text-tertiary-base group-hover:text-white">
-              {node.frontmatter.excerpt}
+              {node?.frontmatter?.excerpt}
             </p>
             <div className="text-secondary-base">{`Continue -->`}</div>
           </div>
