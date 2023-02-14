fbdeeda72bae364b36215334b3a4ff04ce9cf158
hamzabargaz <hamza.bargaz@gmail.com>
2023-02-14 20:53:23 +0100

    add icon arrow/wave-hand

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as Sun } from "./ui/sun";
 export { default as Moon } from "./ui/moon";
+export { default as WavingHand } from "./ui/waving-hand";
+export { default as ArrowRight } from "./ui/arrow-right";
