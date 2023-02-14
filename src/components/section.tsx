8b6c54acb079909db1699f61eba0b3ecde381de4
hamzabargaz <hamza.bargaz@gmail.com>
2023-02-14 21:56:16 +0100

    updates UI

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import cx from "classnames";
 
 type Props = {
   children: React.ReactNode;
+  className?: string;
 };
 
-export default function section({ children }: Props) {
+export default function section({ children, className }: Props) {
   return (
-    <div className="w-full max-w-screen-lg mx-auto my-0 py-0 px-10">
+    <div
+      className={cx(
+        className,
+        "w-full max-w-screen-lg mx-auto my-0 py-0 px-10"
+      )}
+    >
       {children}
     </div>
   );
