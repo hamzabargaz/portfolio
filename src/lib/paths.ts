5e375514458ae471b8136bf9a3df943a0cc1e0bf
hamzabargaz <hamza.bargaz@gmail.com>
2023-02-15 01:34:34 +0100

    remove contact page

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -1,7 +1,6 @@
 const paths = [
   { href: "/about", title: "About" },
   { href: "/blog", title: "Blog" },
-  { href: "/contact", title: "Get In Touch" },
 ];
 
 export default paths;
--- src/pages/contact.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../components/layout";
-
-type Props = {};
-
-export default function Contact({}: Props) {
-  return <Layout>Contact</Layout>;
-}
