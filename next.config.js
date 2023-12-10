d0ea5cefe5d1d9d9a87ef72e7fde94e9f8542758
hamzabargaz <hamza.bargaz@gmail.com>
2023-12-10 15:04:18 +0100

    implement fetch featured projects

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
       "images.unsplash.com",
       "pbs.twimg.com",
       "s3.us-west-2.amazonaws.com",
+      "prod-files-secure.s3.us-west-2.amazonaws.com",
     ],
     formats: ["image/avif", "image/webp"],
   },
--- src/app/featured.tsx
+++ src/app/featured.tsx
@@ -1,8 +1,9 @@
 import { Badge } from "@/components";
 import { Calendar, Layers, MapPin } from "lucide-react";
 import React from "react";
-import { getBlocks, getFeatured } from "../lib/notion";
+import { getBlocks, getDatabase, getFeatured } from "../lib/notion";
 import { format } from "date-fns";
+import Image from "next/image";
 
 type Props = {};
 
@@ -16,8 +17,8 @@ const getFeaturedProjects = async () => {
 
 export default async function Featured({}: Props) {
   const pageId: any = await getFeaturedProjects();
-  const data = await getBlocks(pageId);
-  const projects = extractProjectData(data[0].children);
+  const database = await getBlocks(pageId);
+  const projects = await getDatabase(database[0].id);
 
   if (!pageId || !projects) {
     return <div />;
@@ -27,41 +28,37 @@ export default async function Featured({}: Props) {
     <>
       <h3 className='mb-6'> Featured Projects</h3>
       <div className=' gap-4 flex flex-col max-h-72 overflow-auto pr-4'>
-        {projects.map((item: any, i: number) => (
-          <Project key={i} {...item} />
-        ))}
+        {projects.map((item: any, i: number) => {
+          return <Project key={i} item={item} />;
+        })}
       </div>
     </>
   );
 }
 
 type TProject = {
-  name: string;
-  description: string;
-  icon?: React.ReactNode;
-  start_date: string;
-  end_date: string;
-  tags: string;
-  location: string;
+  item: any;
 };
 
-const Project = ({
-  name,
-  description,
-  icon: Icon,
-  start_date,
-  end_date,
-  tags,
-  location,
-}: TProject) => {
+const Project = ({ item }: TProject) => {
   const formatDate = (date: string) =>
     date && format(new Date(date), "MMM yyyy");
-  const list_tags = tags.split(",");
+  const name = item?.properties?.name?.title[0]?.plain_text;
+  const description = item?.properties?.description?.rich_text[0]?.plain_text;
+  const start_date = item?.properties?.start_date?.date.start;
+  const end_date = item?.properties?.end_date?.date.start;
+  const location = item?.properties?.location?.rich_text[0]?.plain_text;
+  const list_tags = item?.properties?.tags?.rich_text[0]?.plain_text.split(",");
+  const icon = item?.properties?.icon?.files[0]?.file?.url;
   return (
     <div className='flex flex-col gap-4 rounded-xl bg-light-100 dark:bg-dark-100 p-4'>
       <div className='flex items-start justify-between'>
         <div className='p-2 rounded-xl bg-light-200 dark:bg-dark-200'>
-          {Icon ?? <Layers />}
+          {icon ? (
+            <Image src={icon} width={40} height={40} alt='' />
+          ) : (
+            <Layers />
+          )}
         </div>
         <div className='flex flex-wrap justify-end gap-2 text-xs'>
           <div className='flex items-center gap-2'>
@@ -87,23 +84,3 @@ const Project = ({
     </div>
   );
 };
-
-function extractProjectData(jsonData: any) {
-  const projects: any = [];
-
-  jsonData.forEach((item: any) => {
-    const cells = item.table_row.cells;
-
-    const project = {
-      name: cells[0][0].plain_text,
-      start_date: cells[1][0].plain_text,
-      end_date: cells[2][0].plain_text,
-      description: cells[4][0].plain_text,
-      location: cells[3][0].plain_text,
-      tags: cells[5][0].plain_text,
-    };
-    projects.push(project);
-  });
-
-  return projects;
-}
