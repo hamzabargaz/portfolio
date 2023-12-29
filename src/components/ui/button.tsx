5f6fd7d062d605862c0c5148c4294bbfccac48c5
Hamza Bargaz <17603445+hamzabargaz@users.noreply.github.com>
2023-12-30 00:45:22 +0100

    fixes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -31,7 +31,7 @@ const styles = {
   primary: cx(
     "border border-transparent dark:hover:!border-light-400 hover:!border-dark-400",
     "bg-dark-400  dark:bg-light-400 hover:!bg-transparent ",
-    "text-light-400 dark:text-dark-400 dark:hover:text-light-400 hover:text-dark-400"
+    "text-light-100 dark:text-dark-400 dark:hover:text-light-100 hover:text-dark-400"
   ),
   secondary: cx(
     "text-dark-400 dark:text-light-400 hover:bg-dark-400/5 dark:hover:bg-light-400/10"
